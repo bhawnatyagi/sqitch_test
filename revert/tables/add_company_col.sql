ee682bc86810b6fe2b0b73ad223cdd705c73566a
Bhawna Tyagi <btyagi@Bhawna-Tyagi.local>
2022-03-05 18:35:51 -0800

    Add one more column.

---
+++ revert/tables/add_company_col.sql
@@ -0,0 +1,5 @@
+-- Revert snowflake_stg:tables/add_company_col from snowflake
+
+USE WAREHOUSE &warehouse;
+
+-- XXX Add DDLs here.
---
+++ verify/tables/add_company_col.sql
@@ -0,0 +1,5 @@
+-- Verify snowflake_stg:tables/add_company_col on snowflake
+
+USE WAREHOUSE &warehouse;
+
+-- XXX Add verifications here.
